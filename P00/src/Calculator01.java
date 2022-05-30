f7a2c590b8ee2d527b430df86f4d7a74474c576a
21027985 <21027985@PFT-18012892>
2022-05-30 15:21:19 +0800

    First commit to P00

---
+++ P00/src/Calculator01.java
@@ -0,0 +1,33 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21027985, 30 May 2022 12:09:07 pm
+ */
+
+/**
+ * @author 21027985
+ *
+ */
+public class Calculator01 {
+
+	public int add(int a, int b) {
+	    return a + b;
+	  }
+	  
+	  public int sub(int a, int b) {
+	    return a - b;
+	  }
+	  
+	  public int mul(int a, int b) {
+	    return a * b;
+	  }
+	  
+	  public int div(int a, int b) {
+	    return a / b;
+	  }
+
+	}
+
+
---
+++ P00/test/Calculator01Test.java
@@ -0,0 +1,87 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class Calculator01Test {
+int a, b;
+Calculator01 cal;
+
+  @Before
+  public void setUp() throws Exception {
+    a = 4321;
+    b = 1234;
+    cal = new Calculator01();
+
+  }
+
+  @Test
+  // Unit test for addition
+  public void testadd() {
+    
+
+    int actual = cal.add(a,b);
+    int expected = 5555;
+    assertEquals(expected,actual);
+
+  }
+  
+  @Test
+  // Unit test for subtraction
+  public void testsub() {
+    
+    // Arrange
+    int a = 4321;
+    int b = 1234;
+    Calculator01 cal = new Calculator01();
+    
+    // Actual
+    int actual = cal.sub(a,b);
+    int expected = 3087;
+    
+    // Assertion
+    assertEquals(expected,actual);
+  }
+  
+  @Test
+  // Unit test for multiplication
+  public void testmul() {
+    
+    // Arrange
+    int a = 4321;
+    int b = 1234;
+    Calculator01 cal = new Calculator01();
+    
+    // Actual
+    int actual = cal.mul(a,b);
+    int expected = 5332114;
+    
+    // Assertion
+    assertEquals(expected,actual);
+  }
+  
+  @Test
+  // Unit test for division
+  public void testdiv() {
+    
+    // Arrange
+    int a = 4321;
+    int b = 1234;
+    Calculator01 cal = new Calculator01();
+    
+    // Actual
+    int actual = cal.div(a,b);
+    int expected = 3;
+    
+    // Assertion
+    assertEquals(expected,actual);
+  }
+  @After
+  public void tearDown() throws Exception {
+  }
+
+}
